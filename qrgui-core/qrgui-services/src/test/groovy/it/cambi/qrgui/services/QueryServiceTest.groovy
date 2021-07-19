0c750186bb951e231a99bf6c3c2a3d3c2228159f
LUCA <lucacambi77@gmail.com>
2021-07-19 17:08:20 +0200

    adds query service tests

---
+++ qrgui-core/qrgui-services/src/test/groovy/it/cambi/qrgui/services/QueryServiceTest.groovy
@@ -0,0 +1,28 @@
+package it.cambi.qrgui.services
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import it.cambi.qrgui.dao.generic.impl.FirstGenericDao
+import it.cambi.qrgui.model.Temi15UteQue
+import it.cambi.qrgui.query.model.QueryToJson
+import spock.lang.Specification
+
+class QueryServiceTest extends Specification {
+
+    def objectMapper = Mock(ObjectMapper)
+    def queryService = new QueryService(objectMapper)
+    def temi15UteQue = Mock(Temi15UteQue)
+    def firstGenericDao = Mock(FirstGenericDao)
+
+    def "fail validation when statement is null"() {
+        given:
+        QueryToJson queryToJson = new QueryToJson();
+        objectMapper.readValue(temi15UteQue.getJson(), QueryToJson.class) >> queryToJson
+
+        when:
+        def wrapperResponse = queryService.checkQuery(temi15UteQue, false, firstGenericDao)
+
+        then:
+        !wrapperResponse.isSuccess()
+    }
+
+}
